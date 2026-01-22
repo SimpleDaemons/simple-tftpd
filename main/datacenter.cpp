80d55f9074b4619cdc1c52e39baf06e625d76aaf
Brandon L. Burns <brandon@blburns.com>
2026-01-21 20:23:32 -0600

    feat: Add enterprise and datacenter main entry points

---
+++ main/datacenter.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file datacenter.cpp
+ * @brief Main entry point for Simple TFTP Daemon Datacenter Version
+ * @author SimpleDaemons
+ * @copyright 2024 SimpleDaemons
+ * @license BSL 1.1
+ */
+
+// Datacenter version main - placeholder
+// This will include datacenter-specific initialization
+
+int main(int argc, char* argv[]) {
+    // Datacenter version implementation coming soon
+    return 0;
+}
---
+++ main/enterprise.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file enterprise.cpp
+ * @brief Main entry point for Simple TFTP Daemon Enterprise Version
+ * @author SimpleDaemons
+ * @copyright 2024 SimpleDaemons
+ * @license BSL 1.1
+ */
+
+// Enterprise version main - placeholder
+// This will include enterprise-specific initialization
+
+int main(int argc, char* argv[]) {
+    // Enterprise version implementation coming soon
+    return 0;
+}
